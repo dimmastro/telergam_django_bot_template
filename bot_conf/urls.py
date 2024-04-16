0be7bd13747421ae108f6b77268f844aa14e402f
dimmastro <k-d@yandex.ru>
2024-04-16 18:30:59 +0500

    issue-3 fix static css, js files at http://127.0.0.1:8001/admin/

--- bot_conf/urls.py
+++ bot_conf/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 
     # path('payment/', include(('payment.urls', 'payment'), namespace='payment')),
 
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
